13b069a8f15e377a6715d8d0ce5c5751609be23e
Núria Sancho <nuria.sancho@creublanca.es>
2023-06-05 12:59:45 +0200

    [MIG][14.0] lims: Migration to 14.0

--- lims/__manifest__.py
+++ lims/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Lims",
     "summary": """
         Laboratory Information Management System""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/tegin/lims",
